dc9f7247fbb50bec208e26f5bbac2343b4d08715
Ensil <lasags@naver.com>
2024-03-27 18:09:47 +0900

    UPDATE 스타일

--- src/components/Restart.tsx
+++ src/components/Restart.tsx
@@ -4,7 +4,7 @@ interface RestartProps {
 
 export const Restart = ({ onRestartClick }: RestartProps) => {
     return (
-        <div style={{ marginTop: '15px' }}>
+        <div className="restart">
             <button style={{ width: '100%', backgroundColor: 'green', color: 'white' }} onClick={onRestartClick}>
                 Restart?
             </button>
