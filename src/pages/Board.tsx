c375bb2547dc47b9a8bdc161d9b213c49d5a439b
Ensil <lasags@naver.com>
2024-03-29 10:44:07 +0900

    UPDATE 3*3 -> 4*4 틱택토게임

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Square } from '../components/Square';
 import { Restart } from '../components/Restart';
 import { calculateWinner } from '../utils/calculateWinner';
 
 export const Board = () => {
     const [xIsNext, setXIsNext] = useState(true);
-    const [squares, setSquares] = useState<Array<null | string>>(Array(9).fill(null));
+    const [squares, setSquares] = useState<Array<null | string>>(Array(16).fill(null));
 
     function handleClick(index: number) {
         if (squares[index] || calculateWinner(squares)) return;
@@ -22,6 +22,8 @@ export const Board = () => {
         setSquares(Array(9).fill(null));
         setXIsNext(true);
     }
+
+    // console.log(squares)
 
     const winner = calculateWinner(squares);
 
@@ -43,19 +45,28 @@ export const Board = () => {
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
                 <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
             </div>
             <div className='board-row'>
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
                 <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
                 <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-            </div>
-            <div className='board-row'>
                 <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
                 <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+            </div>
+            <div className='board-row'>
                 <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+                <Square value={squares[9]} onSquareClick={() => handleClick(9)} />
+                <Square value={squares[10]} onSquareClick={() => handleClick(10)} />
+                <Square value={squares[11]} onSquareClick={() => handleClick(11)} />
+            </div>
+            <div className='board-row'>
+                <Square value={squares[12]} onSquareClick={() => handleClick(12)} />
+                <Square value={squares[13]} onSquareClick={() => handleClick(13)} />
+                <Square value={squares[14]} onSquareClick={() => handleClick(14)} />
+                <Square value={squares[15]} onSquareClick={() => handleClick(15)} />
             </div>
 
-            {winner || (status === 'Tie Game' && <Restart onRestartClick={handleRestartClick} />)}
+            {(winner || status === 'Tie Game') && <Restart onRestartClick={handleRestartClick} />}
         </>
     );
 };
